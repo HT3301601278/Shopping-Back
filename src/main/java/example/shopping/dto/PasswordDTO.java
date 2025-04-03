2111f7308b20623a5492dc02d02d5ee3d1263356
世间微斯人 <3301601278@qq.com>
2025-04-03 16:50:51 +0800

    完善

---
+++ src/main/java/example/shopping/dto/PasswordDTO.java
@@ -0,0 +1,20 @@
+package example.shopping.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * 密码修改数据传输对象
+ */
+@Data
+public class PasswordDTO {
+    
+    @NotBlank(message = "旧密码不能为空")
+    private String oldPassword;
+    
+    @NotBlank(message = "新密码不能为空")
+    @Size(min = 6, max = 20, message = "密码长度必须在6-20个字符之间")
+    private String newPassword;
+} 
